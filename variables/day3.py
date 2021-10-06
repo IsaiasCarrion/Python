210e2186b874a05ae841abc2fa32050b575cb2cb
IsaiasCarrion <carrionisaias@gmail.com>
2021-10-05 22:03:26 -0300

    cambio de pc

--- variables/day3.py
+++ variables/day3.py
@@ -1,17 +1,34 @@
-edad = 32
-altura = 1,77
-numero_complejo = 32j
+# edad = 32
+# altura = 1,77
+# numero_complejo = 32j
 
-base = int(input('Ingrese la base: '))
-altura = int(input('Ingrese la altura: '))
-resultado = (base*altura)/2
+# base = int(input('Ingrese la base: '))
+# altura = int(input('Ingrese la altura: '))
+# resultado = (base*altura)/2
 
-print(f'el area del triangulo {resultado}:  ')
+# print(f'el area del triangulo {resultado}:  ')
 
-side_a = int(input('Ingrese el lado A: '))
-side_b = int(input('Ingrese el lado B: '))
-side_c = int(input('Ingrese el lado C: '))
-perimetro = side_a + side_b + side_c
+# side_a = int(input('Ingrese el lado A: '))
+# side_b = int(input('Ingrese el lado B: '))
+# side_c = int(input('Ingrese el lado C: '))
+# perimetro = side_a + side_b + side_c
 
-print(perimetro)
+# print(perimetro)
 
+# side_a = int(input('Ingrese la base: '))
+# side_b = int(input('Ingrese la altura: '))
+# resultado = side_a * side_b
+# resultado2 = (side_a + side_b) * 2
+
+# print(resultado, resultado2)
+
+# horas_trabajadas = int(input('Ingrese catidad de horas trabajadas: '))
+# horas = int(input('Tarifa por hora: '))
+# resultado = horas_trabajadas * horas
+
+# print(f'Su ganancia semanal es de: ${resultado} pesos')
+
+edad = int(input('Ingrese la edad: '))
+resultado = edad * 1557600
+
+print(f'Ustede a vivido {resultado} segundos')
