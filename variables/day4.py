51935b1fde3b519e2399b236a0bc49acfbcd1577
IsaiasCarrion <carrionisaias@gmail.com>
2021-10-07 19:17:05 -0300

    actulizando

--- variables/day4.py
+++ variables/day4.py
@@ -9,9 +9,13 @@ print(treinta, ' ' + dias, ' ' + de,' '+ python )
 
 #3)Declare una variable denominada empresa y asígnele un valor inicial "Codificación para todos".
 
-empresa = 'Codificacion para todos'
+empresa = 'Codificacion \n para todos'
 print(empresa)
 print(len(empresa))
 print(str.upper(empresa))
 print(str.lower(empresa))
 print(str.capitalize(empresa))
+print(str.title(empresa))
+print(str.swapcase(empresa))
+print(empresa)
+print(empresa.find('m'))
