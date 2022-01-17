2a7a9d932aaccbe5e995031a4e0454f185144f6e
IsaiasCarrion <carrionisaias@gmail.com>
2022-01-17 16:36:27 -0300

    actualizando sudoku

---
+++ Sudoku/app.py
@@ -0,0 +1,42 @@
+from http.client import OK
+
+board = [
+ ["5","3",".",".","7",".",".",".","."],
+ ["6",".",".","1","9","5",".",".","."],
+ [".","9","8",".",".",".",".","6","."],
+ ["8",".",".",".","6",".",".",".","3"],
+ ["4",".",".","8",".","3",".",".","1"],
+ ["7",".",".",".","2",".",".",".","6"],
+ [".","6",".",".",".",".","2","8","."],
+ [".",".",".","4","1","9",".",".","5"],
+ [".",".",".",".","8",".",".","7","9"]
+]
+
+class ValidateSudoku:
+    def __init__(self, tablero) -> None:
+        self.tablero = tablero
+        self.lista_invertida = list()
+
+    def chequeo_general(self):
+        assert len(self.tablero) == 9, "El Tablero ingresado no respeta 9 x 9 "
+        for fila in self.tablero:
+            assert len(fila) == 9
+
+    def chequeo_filas(self, lista_a_checkear='tablero_general'):
+        if lista_a_checkear == 'tablero_general':
+            lista_a_checkear = self.tablero
+        for fila in lista_a_checkear:
+            for elemento in fila:
+                if elemento != '.':
+                    assert fila.count(elemento) ==1
+
+    def chequeo_columnas(self):
+        for column_index in range (0,9):
+            for row_index in range(0,9):
+                self.lista_invertida.append(self.tablero[row_index][column_index])
+                self.chequeo_filas([self.lista_invertida])
+                self.lista_invertida.clear()
+
+sudoku = ValidateSudoku(board)
+sudoku.chequeo_general()
+sudoku.chequeo_filas()
