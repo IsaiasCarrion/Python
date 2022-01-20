1d2c89f18661d5ebad9d1e86ef96dcb16bf99864
Isaias Carrion <carrionisaias@gmail.com>
2022-01-20 20:53:22 -0300

    seguimos aprendiendo

--- universidadPython/POO/Persona.py
+++ universidadPython/POO/Persona.py
@@ -10,21 +10,28 @@
 # print(persona1.edad)
 
 class Persona:
-    def __init__(self, nombre, apellido, edad, *valores, **terminos):
-        self.nombre = nombre
+    def __init__(self, nombre, apellido, edad,):  # *valores, **terminos
+        self._nombre = nombre
         self.apellido = apellido
         self.edad = edad
-        self.valores = valores
-        self.terminos = terminos
+
+    def nombre(self):
+        return self._nombre
 
     def mostrar_detalle(self):
-        print(
-            f'Persona: {self.nombre} {self.apellido} {self.edad} {self.valores} {self.terminos}')
+        print(f'Persona: {self._nombre} {self.apellido} {self.edad}')
 
 
-persona1 = Persona('Juan', 'Perez', 28, '44557766',
-                   2, 3, 4, 5, m='manzana', p='pera')
-persona1.mostrar_detalle()
+# '44557766', 2, 3, 4, 5, m='manzana', p='pera'
+persona1 = Persona('Juan', 'Perez', 28, )
+print(persona1.nombre())
+
+
+# persona1.mostrar_detalle()
+#persona1._nombre = 'Juan Carlos'
+# persona1.mostrar_detalle()
+
+
 # Persona.mostrar_detalle(persona1)
 # print(f'Objeto Persona 1 {persona1.nombre} {persona1.apellido} {persona1.edad}')
 
@@ -33,8 +40,8 @@ persona1.mostrar_detalle()
 # persona1.edad = 25
 # print(f'Objeto Persona 1 {persona1.nombre} {persona1.apellido} {persona1.edad}')
 
-persona2 = Persona('Karla', 'Gomez', 30)
-persona2.mostrar_detalle()
+# persona2 = Persona('Karla', 'Gomez', 30)
+# persona2.mostrar_detalle()
 # print(f'Objeto Persona 2 {persona2.nombre} {persona2.apellido} {persona2.edad}')
 
 # persona2.nombre = 'Macarena'
