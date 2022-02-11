c2fdbda4c650c1bc2c8f14e727ef744d91a7f91e
IsaiasCarrion <carrionisaias@gmail.com>
2022-02-11 06:24:42 -0300

    Python validaciones

--- universidadPython/Herencia/test_figura_geometrica.py
+++ universidadPython/Herencia/test_figura_geometrica.py
@@ -1,10 +1,14 @@
 from Cuadrado import Cuadrado
+from Rectangulo import Rectangulo
 
 cuadrado1 = Cuadrado(5, 'rojo')
 print(f'Calculo del Area es: {cuadrado1.calcular_area()}')
 print(cuadrado1)
 
 
+rectangulo1 = Rectangulo(3, 8, 'verde')
+print(f'Calculo del Area es: {rectangulo1.calcular_area()}')
+print(rectangulo1)
 # MRO Method Resolution Order
 
 # Cuadrado.mro()
