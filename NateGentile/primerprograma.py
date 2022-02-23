e96fef1d16430768c0c1c6cbfdf9007c02f3f330
Isaias Carrion <carrionisaias@gmail.com>
2022-02-23 04:49:02 -0300

    volviendo

---
+++ NateGentile/primerprograma.py
@@ -0,0 +1,10 @@
+a = 2
+print(a)
+
+a = a * 2
+print(a)
+
+a = "hola"
+print(a)
+
+print(a * 3)
---
+++ WebScraping/pandas.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+pd.read_csv('https://www.football-data.co.uk/mmz4281/2122/E0.csv')
