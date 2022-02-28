7b15758faee5b8d0ac39206db625f8d107909463
IsaiasCarrion <carrionisaias@gmail.com>
2022-02-28 16:31:30 -0300

    retomando python

--- NateGentile/primerprograma.py
+++ NateGentile/primerprograma.py
@@ -1,3 +1,5 @@
+
+'''
 a = 2
 print(a)
 
@@ -8,3 +10,20 @@ a = "hola"
 print(a)
 
 print(a * 3)
+
+nombre = input('Cual es tu nombre?: ')
+print('Tu nombre es: ' + nombre)
+
+maximoValor = max(1, 2, 3, 4)
+print(maximoValor)
+'''
+
+a = int(input('Ingrese el primer numero: '))
+b = int(input('Ingrese el segundo numero: '))
+c = int(input('Ingrese el tercer numero: '))
+
+maximo_valor = max(a, b, c)
+minimo_valor = min(a, b, c)
+
+print(f'el maximo valor es {maximo_valor}')
+print(f'el minimo valor es {minimo_valor}')
