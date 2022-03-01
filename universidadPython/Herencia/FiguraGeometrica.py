f08288a2317e1a6aeb7b38c33c3ebd436045c0c0
Isaias Carrion <carrionisaias@gmail.com>
2022-02-28 21:48:10 -0300

    python

--- universidadPython/Herencia/FiguraGeometrica.py
+++ universidadPython/Herencia/FiguraGeometrica.py
@@ -1,7 +1,13 @@
 class FiguraGeometrica:
     def __init__(self, alto, ancho):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+        if self._validar_valor(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
 
     @property
     def ancho(self):
@@ -21,3 +27,6 @@ class FiguraGeometrica:
 
     def __str__(self):
         return f'Figura geometrica[Ancho: {self._ancho}, Alto: {self._alto}]'
+
+    def _validar_valor(self, valor):
+        return True if 0 < valor < 10 else False
--- universidadPython/Herencia/test_figura_geometrica.py
+++ universidadPython/Herencia/test_figura_geometrica.py
@@ -1,12 +1,12 @@
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
 
-cuadrado1 = Cuadrado(5, 'rojo')
+cuadrado1 = Cuadrado(lado=-5, color='rojo')
 print(f'Calculo del Area es: {cuadrado1.calcular_area()}')
 print(cuadrado1)
 
 
-rectangulo1 = Rectangulo(3, 8, 'verde')
+rectangulo1 = Rectangulo(ancho=13, alto=8, color='verde')
 print(f'Calculo del Area es: {rectangulo1.calcular_area()}')
 print(rectangulo1)
 # MRO Method Resolution Order
