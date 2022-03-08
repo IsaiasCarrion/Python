493c507d0f7ba025e93f64d8851006775cce6c1f
Isaias Carrion <carrionisaias@gmail.com>
2022-03-08 17:51:57 -0300

    listas python

---
+++ NateGentile/lista_de_la_compra.py
@@ -0,0 +1,10 @@
+'''
+Programa lista de la compra
+
+¿Que desea comprar? ([Q] para salir) > Leche
+Seguro q quiere añadir "Leche"? [S/N] > S
+Leche Añadida!
+
+La lista de la compra es:
+["Leche", "Pan"]
+'''
--- NateGentile/test_while.py
+++ NateGentile/test_while.py
@@ -1,7 +1,13 @@
+import os
 from random import randint
 
-vida_pikachu = 80
-vida_squirtle = 90
+
+vida_inicial_pikachu = 80
+vida_inicial_squirtle = 90
+tamaño_vida = 20
+
+vida_pikachu = vida_inicial_pikachu
+vida_squirtle = vida_inicial_squirtle
 
 while vida_pikachu > 0 and vida_squirtle > 0:
     # Se desenvuelven los turnos de combate
@@ -17,18 +23,34 @@ while vida_pikachu > 0 and vida_squirtle > 0:
         print("Pikachu ataque Onda Trueno")
         vida_squirtle -= 11
 
+    if vida_squirtle < 0:
+        vida_squirtle = 0
+    if vida_pikachu < 0:
+        vida_pikachu = 0
+
     # Vida Pokemons
     print("La vida de Pikachu {}, La vida de Squirtle {}".format(
         vida_pikachu, vida_squirtle))
 
+    baras_de_vida_pikachu = int(
+        vida_pikachu * tamaño_vida / vida_inicial_pikachu)
+    print("Pikachu: [{}{}] ({}/{})".format(
+        "*" * baras_de_vida_pikachu, " " * (tamaño_vida - baras_de_vida_pikachu), vida_pikachu, vida_inicial_pikachu))
+
+    baras_de_vida_squirtle = int(
+        vida_squirtle * tamaño_vida / vida_inicial_squirtle)
+    print("Squirtle: [{}{}] ({}/{})".format(
+        "*" * baras_de_vida_squirtle, " " * (tamaño_vida - baras_de_vida_squirtle), vida_squirtle, vida_inicial_squirtle))
+
     input("Enter para continuar ...")
+    os.system("cls")
 
     # Turno Squirtle
     print("Turno Squirtle")
     ataque_squirtle = None
-    while ataque_squirtle != "P" and ataque_squirtle != "A" and ataque_squirtle != "B":
+    while ataque_squirtle not in ["P", "A", "B", "T"]:
         ataque_squirtle = input(
-            "Que ataque deseas realizar: [P]lacaje, Pistola de [A]gua, [B]urbuja: ")
+            "Que ataque deseas realizar: [P]lacaje, Pistola de [A]gua, [B]urbuja, Pasar [T]urno: ")
 
     if ataque_squirtle == "P":
         vida_pikachu -= 10
@@ -36,12 +58,22 @@ while vida_pikachu > 0 and vida_squirtle > 0:
         vida_pikachu -= 12
     elif ataque_squirtle == "B":
         vida_pikachu -= 9
+    elif ataque_squirtle == "T":
+        print("Pasaste Turno")
 
     # Vida Pokemons
-    print("La vida de Pikachu {}, La vida de Squirtle {}".format(
-        vida_pikachu, vida_squirtle))
+    baras_de_vida_pikachu = int(
+        vida_pikachu * tamaño_vida / vida_inicial_pikachu)
+    print("Pikachu: [{}{}] ({}/{})".format(
+        "*" * baras_de_vida_pikachu, " " * (tamaño_vida - baras_de_vida_pikachu), vida_pikachu, vida_inicial_pikachu))
+
+    baras_de_vida_squirtle = int(
+        vida_squirtle * tamaño_vida / vida_inicial_squirtle)
+    print("Pikachu: [{}{}] ({}/{})".format(
+        "*" * baras_de_vida_squirtle, " " * (tamaño_vida - baras_de_vida_squirtle), vida_squirtle, vida_inicial_squirtle))
 
     input("Enter para continuar ...")
+    os.system("cls")
 
 if vida_pikachu > vida_squirtle:
     print("Gana Pikachu")
