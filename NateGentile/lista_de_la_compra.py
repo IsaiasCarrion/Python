0cb47c04363545da031c318cf834ef874e9f8fc2
IsaiasCarrion <carrionisaias@gmail.com>
2022-03-09 09:41:35 -0300

    bucle for

--- NateGentile/lista_de_la_compra.py
+++ NateGentile/lista_de_la_compra.py
@@ -8,3 +8,23 @@ Leche Añadida!
 La lista de la compra es:
 ["Leche", "Pan"]
 '''
+
+lista_compras = []
+input_de_usuario = None
+
+print("Lista de la compra")
+
+
+while input_de_usuario != "Q":
+    input_de_usuario = input("Que desea comprar? ([Q] para salir): ")
+    if input_de_usuario == "Q":
+        pass
+    elif input_de_usuario in lista_compras:
+        print("{} esto ya esta en la lista".format(input_de_usuario))
+    else:
+        if input("Seguro q quiere añadir {} a la lista de la compras? [S/N]: ".format(input_de_usuario)) == "S":
+            lista_compras.append(input_de_usuario)
+
+
+print("La lista de la compra es: ")
+print(lista_compras)
