8b7bdffa99aa6cc790aedb9023a908666ab868ca
IsaiasCarrion <carrionisaias@gmail.com>
2022-03-15 07:35:46 -0300

    aplicando el conocimiento

--- NateGentile/maze.py
+++ NateGentile/maze.py
@@ -1,45 +1,82 @@
 import os
+import random
 import readchar
 
 POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
+NUM_OF_MAP_OBJECTS = 11
 
-my_position = [5, 6]
+my_position = [6, 3]
+tail_length = 0
+tail = []
+map_objects = []
 
-map_objects = [[2, 3], [5, 4], [3, 4], [10, 6]]
+# Generate random objects on the map
+while len(map_objects) < NUM_OF_MAP_OBJECTS:
+    new_position = [random.randint(0, MAP_WIDTH), random.randint(0, MAP_HEIGHT)]
 
+    if new_position not in map_objects and new_position != my_position:
+        map_objects.append(new_position)
 
+# print(map_objects)
+
+# Main loop
 while True:
     # Draw map
     print("+" + "-" * MAP_WIDTH * 3 + "+")
 
     for coordinate_y in range(MAP_HEIGHT):
         print("|", end="")
+
         for coordinate_x in range(MAP_WIDTH):
+
+            char_to_draw = " "
+            object_in_cell = None
+            tail_in_cell = None
+
+            for map_object in map_objects:
+                if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
+                    char_to_draw = "*"
+                    object_in_cell = map_object
+
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
-                print(" @ ", end="")
-            else:
-                print("   ", end="")
+                char_to_draw = "@"
+
+                if object_in_cell:
+                    map_objects.remove(object_in_cell)
+                    tail_length += 1
+
+            print(" {} ".format(char_to_draw), end="")
         print("|")
 
     print("+" + "-" * MAP_WIDTH * 3 + "+")
+    print(tail)
+    print(tail_length)
 
     # Ask user where he wants to move
     # direction = input("¿Donde te quieres mover? [WASD]: ")
     direction = readchar.readchar().decode()
 
     if direction == "w":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_Y] -= 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction == "s":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_Y] += 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction == "a":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_X] -= 1
         my_position[POS_X] %= MAP_WIDTH
-    elif direction ==  "d":
+    elif direction == "d":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_X] += 1
         my_position[POS_X] %= MAP_WIDTH
     elif direction == "q":
