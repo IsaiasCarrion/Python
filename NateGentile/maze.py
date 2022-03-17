8f741cd8b693e682998767bb90dbc2e0af9ccff3
Isaias Carrion <carrionisaias@gmail.com>
2022-03-17 19:06:50 -0300

    testing

--- NateGentile/maze.py
+++ NateGentile/maze.py
@@ -4,29 +4,37 @@ import readchar
 
 POS_X = 0
 POS_Y = 1
-MAP_WIDTH = 20
-MAP_HEIGHT = 15
 NUM_OF_MAP_OBJECTS = 11
+
+obstacle_definition = """
+"""
+
 
 my_position = [6, 3]
 tail_length = 0
 tail = []
 map_objects = []
 
-# Generate random objects on the map
-while len(map_objects) < NUM_OF_MAP_OBJECTS:
-    new_position = [random.randint(
-        0, MAP_WIDTH), random.randint(0, MAP_HEIGHT)]
+end_game = False
+died = False
 
-    if new_position not in map_objects and new_position != my_position:
-        map_objects.append(new_position)
-
-# print(map_objects)
-
+# Create obstacle map
+obstacle_definition = [list(row) for row in obstacle_definition.split("\n")]
+MAP_WIDTH = len(obstacle_definition[0])
+MAP_HEIGHT = len(obstacle_definition)
 # Main loop
-while True:
+while not end_game:
+    os.system("clear")
+    # Generate random objects on the map
+    while len(map_objects) < NUM_OF_MAP_OBJECTS:
+        new_position = [random.randint(
+            0, MAP_WIDTH), random.randint(0, MAP_HEIGHT)]
+
+        if new_position not in map_objects and new_position != my_position:
+            map_objects.append(new_position)
+
     # Draw map
-    print("+" + "-" * MAP_WIDTH * 3 + "+")
+    print("+" + "-" * MAP_WIDTH * 2 + "+")
 
     for coordinate_y in range(MAP_HEIGHT):
         print("|", end="")
@@ -39,20 +47,32 @@ while True:
 
             for map_object in map_objects:
                 if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
-                    char_to_draw = "*"
+                    char_to_draw = " *"
                     object_in_cell = map_object
 
+            for tail_piece in tail:
+                if tail_piece[POS_X] == coordinate_x and tail_piece[POS_Y] == coordinate_y:
+                    char_to_draw = " @"
+                    tail_in_cell = tail_piece
+
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
-                char_to_draw = "@"
+                char_to_draw = " @"
 
                 if object_in_cell:
                     map_objects.remove(object_in_cell)
                     tail_length += 1
 
-            print(" {} ".format(char_to_draw), end="")
+                if tail_in_cell:
+                    end_game = True
+                    died = True
+
+            if obstacle_definition[coordinate_y][coordinate_x] == "#":
+                char_to_draw = "###"
+
+            print("{}".format(char_to_draw), end="")
         print("|")
 
-    print("+" + "-" * MAP_WIDTH * 3 + "+")
+    print("+" + "-" * MAP_WIDTH * 2 + "+")
     print(tail)
     print(tail_length)
 
@@ -61,21 +81,33 @@ while True:
     direction = readchar.readchar().decode()
 
     if direction == "w":
+        new_position = [my_position[POS_X],
+                        (my_position[POS_Y] - 1) % MAP_WIDTH]
+
         tail.insert(0, my_position.copy())
         tail = tail[:tail_length]
         my_position[POS_Y] -= 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction == "s":
+        new_position = [my_position[POS_X],
+                        (my_position[POS_Y] + 1) % MAP_WIDTH]
+
         tail.insert(0, my_position.copy())
         tail = tail[:tail_length]
         my_position[POS_Y] += 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction == "a":
+        new_position = [(my_position[POS_X] - 1) %
+                        MAP_WIDTH, my_position[POS_Y]]
+
         tail.insert(0, my_position.copy())
         tail = tail[:tail_length]
         my_position[POS_X] -= 1
         my_position[POS_X] %= MAP_WIDTH
     elif direction == "d":
+        new_position = [(my_position[POS_X] + 1) %
+                        MAP_WIDTH, my_position[POS_Y]]
+
         tail.insert(0, my_position.copy())
         tail = tail[:tail_length]
         my_position[POS_X] += 1
@@ -83,4 +115,5 @@ while True:
     elif direction == "q":
         break
 
-    os.system("cls")
+if died:
+    print("Has Muerto")
