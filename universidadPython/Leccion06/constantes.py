f346d756e6deecd741a0384cc0718fd9e22fa096
Isaias Carrion <carrionisaias@gmail.com>
2022-03-26 01:23:05 -0300

    constantes.py

---
+++ universidadPython/Leccion06/constantes.py
@@ -0,0 +1,1 @@
+MI_CONSTANTE = 'Valor de mi constante'
--- universidadPython/Tipos de datos/funciones.py
+++ universidadPython/Tipos de datos/funciones.py
@@ -1,13 +1,13 @@
-#Funciones syntaxis
+# Funciones syntaxis
 def nombre_de_function(nombre):
-    #corremos codigo
+    # corremos codigo
     print(f'Las funciones retornan algo: {nombre}')
+
 
 nombre_de_function('Isaias')
 
-#Las funciones siempre return para asignar el output de la funcion
+# Las funciones siempre return para asignar el output de la funcion
+
 
 def decirHola():
     print('Hola')
-
-
