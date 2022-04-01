6bc4385185fc6313fc73b92c1b71bc901f347b9b
Isaias Carrion <carrionisaias@gmail.com>
2022-04-01 18:19:23 -0300

    practicando

---
+++ universidadPython/catalogo_peliculas/test_catalogo_peliculas.py
@@ -0,0 +1,9 @@
+opcion = None
+
+while opcion != 4:
+    print('Opciones: ')
+    print('1. Agregar Peliculas: ')
+    print('2. Listarr Peliculas: ')
+    print('3. Eliminar Catalogos de Peliculas: ')
+    print('4. Salir')
+    opcion = input('Escribe tu opcion 1-4: ')
